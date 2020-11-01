5de10e8fd213af00bbeb51438333ac979c17625f
DeveloperPyJS <47751143+DeveloperPyJS@users.noreply.github.com>
2020-11-01 18:51:42 +0300

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -10,26 +10,7 @@ $( document ).ready(function() {
 });
 
 function signIn() {
-	firebase.auth().signInWithPopup(provider).then(function(result) {
-	  // This gives you a Google Access Token. You can use it to access the Google API.
-	  var token = result.credential.accessToken;
-	  // The signed-in user info.
-	   user = result.user;
-	  showWelcomeContainer();
-	  sessionStorage.token = token;
-	  // ...
-	}).catch(function(error) {
-	  // Handle Errors here.
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
-	  // The email of the user's account used.
-	  var email = error.email;
-	  // The firebase.auth.AuthCredential type that was used.
-	  var credential = error.credential;
-	  // ...
-		console.log(user)
-		provider.setCustomParameters({
-    'name': 'Введите ваше имя'
+continue
 });
 		// Step 1.
 // User tries to sign in to Google.
