5a5a4d08d99465cdbe3b63476efc787a8862a892
DeveloperPyJS <47751143+DeveloperPyJS@users.noreply.github.com>
2020-11-01 19:29:27 +0300

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -89,6 +89,26 @@ auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).catch(function(erro
 
 };
 
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // ...
+  } else {
+		firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});
+  }
+});
+
 function showWelcomeContainer() {
 	$("#login").hide();
 	$("#welcome").show();
@@ -145,4 +165,3 @@ function uploadFile() {
 	});
 
 }
-alertr(user.email)
