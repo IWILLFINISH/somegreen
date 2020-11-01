f332ba1b259aad1f60ff730911193bd7b74ffc89
DeveloperPyJS <47751143+DeveloperPyJS@users.noreply.github.com>
2020-11-01 18:57:59 +0300

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -10,7 +10,26 @@ $( document ).ready(function() {
 });
 
 function signIn() {
-continue
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	   user = result.user;
+	  showWelcomeContainer();
+	  sessionStorage.token = token;
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+		console.log(user)
+		provider.setCustomParameters({
+    'name': 'Введите ваше имя'
 });
 		// Step 1.
 // User tries to sign in to Google.
@@ -74,7 +93,7 @@ function showWelcomeContainer() {
 	$("#login").hide();
 	$("#welcome").show();
 	$(".upload-group").show();
-	$("#welcomeText").html("Привет, " + user.displayName);
+	$("#welcomeText").html("Привет, дружище!"");
 };
 
 $(".dropdown").on("hide.bs.dropdown", function(event){
